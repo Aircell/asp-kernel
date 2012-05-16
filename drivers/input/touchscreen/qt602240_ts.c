d761b72afb76f1698d7209e2d8eba16144bdfdc0
tarr <tarr@bouldersystemsdesign.com>
2012-05-16 13:19:03 -0600

    TARR - revert tp old P3 touchscreen orientation for a short while

--- drivers/input/touchscreen/qt602240_ts.c
+++ drivers/input/touchscreen/qt602240_ts.c
@@ -722,7 +722,7 @@ static void qt602240_input_touchevent(struct qt602240_data *data,
 	/* Tarr - Because the touchscreen got "flipped" in the P3, need to
      * adjust x accordingly
 	 */
-	x = 1170 - x;
+	// TARR - for the moment (5/16/2012) leave it alone --x = 1170 - x;
 	y = (message->message[2] << 2) | ((message->message[3] & ~0xf3) >> 2);
 	area = message->message[4];
 
