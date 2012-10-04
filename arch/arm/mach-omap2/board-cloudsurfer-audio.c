9ce95cb3553f5fafb5b96181b1f6c900513bfeb4
John Kelly <jkelly@aircell.com>
2012-10-04 14:03:17 -0600

    Delay earpiece powerup 500ms on answer to limit power surge

--- arch/arm/mach-omap2/board-cloudsurfer-audio.c
+++ arch/arm/mach-omap2/board-cloudsurfer-audio.c
@@ -23,7 +23,17 @@ static void twl_do_unmute(unsigned long data) {
 	pr_debug("Unmuting from the timer function\n");
 	gpio_set_value(AIRCELL_MUTE, 0);
 }
+
+static void twl_do_amp(unsigned long data) {
+	pr_debug("Powering the %s amp from the timer function\n", data?"ringer":"earpiece");
+	if(data) {
+		gpio_set_value(AIRCELL_RINGER_ENABLE, 1);
+	} else {
+		gpio_set_value(AIRCELL_EARPIECE_ENABLE, 1);
+	}	
+}
 DEFINE_TIMER(mute_timer, twl_do_unmute, 0, 0);
+DEFINE_TIMER(amp_timer, twl_do_amp, 0, 0);
 
 void twl4030_unmute_delay(unsigned int delay) {
 	mod_timer(&mute_timer, jiffies + delay);
@@ -106,21 +116,19 @@ EXPORT_SYMBOL(twl4030_get_ext_ringer);
 
 int twl4030_set_ext_ringer(int ringer)
 {
-       int headset = 0;
-       headset = gpio_get_value(AIRCELL_HEADSET_DETECT);
+	int headset = 0;
+	headset = gpio_get_value(AIRCELL_HEADSET_DETECT);
 
-       pr_debug("%s Ringer set to %d (headset %s)\n", __func__, ringer, headset?"in":"out");
+	pr_debug("%s Ringer set to %d (headset %s)\n", __func__, ringer, headset?"in":"out");
 
-       if(ringer) {
-	       gpio_set_value(AIRCELL_EARPIECE_ENABLE, 0);
-		udelay(50);
-	       gpio_set_value(AIRCELL_RINGER_ENABLE, 1);
-       } else {
-	       gpio_set_value(AIRCELL_RINGER_ENABLE, 0);
-		udelay(50);
-	       gpio_set_value(AIRCELL_EARPIECE_ENABLE, 1);
-       }
-       return 0;
+	gpio_set_value(AIRCELL_EARPIECE_ENABLE, 0);
+	gpio_set_value(AIRCELL_RINGER_ENABLE, 0);
+
+	amp_timer.expires = jiffies + 50;
+	amp_timer.data = ringer;
+	add_timer(&amp_timer);
+
+	return 0;
 }
 EXPORT_SYMBOL(twl4030_set_ext_ringer);
 
@@ -129,6 +137,8 @@ EXPORT_SYMBOL(twl4030_set_ext_ringer);
 void cloudsurfer_init_audio(void) {
 	init_timer(&mute_timer);
 	add_timer(&mute_timer);
+
+	init_timer(&amp_timer);
 }
 
 
